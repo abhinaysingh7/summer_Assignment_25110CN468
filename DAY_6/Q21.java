4aa20afca8f146d8b94a6e14996b9c79a3c4ad04
Abhinay Singh <abhinay2005singhrajawat@gmail.com>
2026-06-06 18:49:44 +0530

    Day 6 Assignment

---
+++ DAY_6/Q21.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Q21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        String binary = "";
+
+        while (n > 0) {
+            binary = (n % 2) + binary;
+            n = n / 2;
+        }
+
+        System.out.println(binary);
+    }
+}
---
+++ DAY_6/Q22.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Q22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int binary = sc.nextInt();
+
+        int decimal = 0;
+        int power = 1;
+
+        while (binary > 0) {
+            int digit = binary % 10;
+
+            decimal = decimal + digit * power;
+
+            power = power * 2;
+            binary = binary / 10;
+        }
+
+        System.out.println(decimal);
+    }
+}
