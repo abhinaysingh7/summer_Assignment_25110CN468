aef7956a8fe490ac56bfe28024d2c23fc4bb2287
Abhinay Singh <abhinay2005singhrajawat@gmail.com>
2026-06-14 20:31:15 +0530

    Day 14: Array searching and frequency programs

---
+++ DAY_14/Q56.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Q56 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter elements: ");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Duplicate elements: ");
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(arr[i] == arr[j]) {
+                    System.out.print(arr[i] + " ");
+                    break;
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
