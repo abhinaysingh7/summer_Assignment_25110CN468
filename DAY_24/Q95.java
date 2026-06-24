4ff60577356e88aa72c51f32cd5fd36ed3037ba5
Abhinay Singh <abhinay2005singhrajawat@gmail.com>
2026-06-24 19:16:35 +0530

    Day 24: String rotation, compression, longest word and duplicate removal

---
+++ DAY_24/Q95.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Q95 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter sentence: ");
+        String str = sc.nextLine();
+
+        String[] words = str.split(" ");
+
+        String longest = words[0];
+
+        for (int i = 1; i < words.length; i++) {
+            if (words[i].length() > longest.length()) {
+                longest = words[i];
+            }
+        }
+
+        System.out.println("Longest Word: " + longest);
+
+        sc.close();
+    }
+}
